61fe4e3fb52f2cce36afc2a190c9dbdeb0751d80
dbushenko <d.bushenko@gmail.com>
2014-11-17 19:12:25 +0300

    Updated to newer versions of libraries.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject sitecompiler "0.2.3-SNAPSHOT"
+(defproject sitecompiler "0.2.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.aviso/pretty "0.1.12"]
-                 [markdown-clj "0.9.44"]
+                 [io.aviso/pretty "0.1.13"]
+                 [markdown-clj "0.9.57"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [hiccup "1.0.5"]
-                 [cuma "0.0.8"]
-                 [fleet "0.10.1"]]
+                 [cuma "0.1.0"]
+                 [fleet "0.10.2"]]
   :main sitecompiler.core)
