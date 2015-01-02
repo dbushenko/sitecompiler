416af0a68055af69507843c6dcbcd3a526d8f409
dbushenko <d.bushenko@gmail.com>
2015-01-02 20:05:25 +0300

    Bumped new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sitecompiler "0.2.4-SNAPSHOT"
+(defproject sitecompiler "0.2.5"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
